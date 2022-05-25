8e01d9008a281d18ed8e8c85afc3b37fcebf57b1
Petzi <daboarderpjb@gmail.com>
2022-05-25 10:06:19 +0200

    abb bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="3">
+        <v-toolbar-title> Home </v-toolbar-title>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
