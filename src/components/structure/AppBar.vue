1052f4685be17726d7904193fd2307bd5a90c77c
Petzi <daboarderpjb@gmail.com>
2022-05-25 11:10:37 +0200

    transition

--- src/components/structure/AppBar.vue
+++ src/components/structure/AppBar.vue
@@ -1,33 +1,20 @@
 <template>
-  <v-app-bar app color="primary" dark>
+  <v-app-bar app color="secondary" dark>
     <div class="d-flex align-center">
       <v-img
         alt="Vuetify Logo"
         class="shrink mr-2"
         contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        :src="require('@/assets/logo.png')"
         transition="scale-transition"
-        width="40"
-      />
-
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
+        width="140"
       />
     </div>
 
     <v-spacer></v-spacer>
 
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
+    <v-btn href="https://recipes-petzi.web.app" target="_blank" text>
+      <span class="mr-2">Food Tinder</span>
       <v-icon>mdi-open-in-new</v-icon>
     </v-btn>
   </v-app-bar>
@@ -37,11 +24,7 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class AppBar extends Vue {
-
-}
+export default class AppBar extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,39 @@
         <v-toolbar-title> Home </v-toolbar-title>
       </v-col>
     </v-row>
+    <v-row justify="center">
+      <v-col cols="4">
+        <v-row>
+          <transition-group mout="out-in" name="group">
+            <template v-for="card in itemList">
+              <v-slide-y-transition :key="card" group="group">
+                <v-col cols="3" style="border: 0px solid">
+                  <v-card color="accent" width="100%" class="ma-0">
+                    <v-card-text>{{ card }}</v-card-text>
+                  </v-card>
+                </v-col>
+              </v-slide-y-transition>
+            </template>
+          </transition-group>
+        </v-row>
+        <v-row>
+          <v-col>
+            <template>
+              <v-form ref="input" @submit.prevent="submitItem">
+                <v-text-field
+                  id="item"
+                  label="Zutat"
+                  hint="Mit Enter hinzufügen"
+                  persistent-hint
+                  outlined
+                  v-model="item"
+                ></v-text-field>
+              </v-form>
+            </template>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -17,5 +50,15 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  item = "";
+  itemList: string[] = [];
+
+  submitItem(): void {
+    if (this.item.length > 0) {
+      this.itemList.push(this.item);
+      this.item = "";
+    }
+  }
+}
 </script>
